95772d589ca3fa6d02738d0ba9e02a3f88b63767
MNV-mm <87756291+MNV-mm@users.noreply.github.com>
2023-01-13 12:22:13 +0300

    Update llg_prep.py

--- llg_prep.py
+++ llg_prep.py
@@ -9,10 +9,10 @@ import DD_Hd
 import funcs_2
 #import dolfin as dp
 
-Lx = 60 # 60 150 80
-Ly = 40 # 30 80 40
+Lx = 200 # 60 150 80
+Ly = 100 # 30 80 40
 
 #hd_ext_expr = funcs_2.n_pair(Ly, 30, 10, 9, 3)
 #hd = dp.Expression(hd_ext_expr, degree = 3)
 
-DD_Hd.pe_EF(5,30,1,Lx,Ly,20,'/home/mnv/Documents/python_doc/llg_nl/')
+DD_Hd.pe_EF(5,30,1,Lx,Ly,20,'/home/mnv/llg_nl/')
